260a201cb36496a12596e4a94dea5839d8204597
Siddhi Malwadkar <120126805+SiddhiMalwadkar@users.noreply.github.com>
2025-07-11 18:01:05 +0530

    Create User.java

---
+++ model/User.java
@@ -0,0 +1,19 @@
+package com.example.bookapi.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ElementCollection
+    private List<String> favoriteGenres;
+
+
+}
